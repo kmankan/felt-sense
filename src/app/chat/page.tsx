6f39f468b37d89080fe3759f33f7c9c945e0ab88
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-11-12 15:26:32 -0500

    create store for indicating speaking/listening/thinking and display that status

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,13 +1,12 @@
 import { withAuth } from '@workos-inc/authkit-nextjs';
 import { AudioRecorder } from "../components/AudioRecorder";
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ShowConversationState from "../components/ShowConversationState";
 //mui up arrow
 
 
 // This is now a Server Component (no "use client" directive)
 export default async function ChatPage() {
   const { user } = await withAuth();
-
   return (
     <div>
       <AudioRecorder />
@@ -19,12 +18,16 @@ export default async function ChatPage() {
         </div>
       </div>
       <div className="flex absolute top-1/2 left-1/2 max-sm:bottom-0 max-sm:left-1/2 w-[60%] h-[50%] sm:w-[40%] sm:h-[50%] bg-white rounded-lg transform -translate-x-1/2 sm:-translate-y-28  shadow-lg opacity-50">
-        <button className="rounded-full sm:w-24 sm:h-24 w-16 h-16 bg-blue-500 self-end mx-auto mb-10" style={{ aspectRatio: '1 / 1' }}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-arrow-up" style={{ transform: 'scale(0.5)' }}>
-            <line x1="12" y1="19" x2="12" y2="5"></line>
-            <polyline points="5 12 12 5 19 12"></polyline>
-          </svg>
-        </button>
+        <div className="flex flex-col items-center justify-center self-end mx-auto">
+          <ShowConversationState />
+
+          <button className="rounded-full sm:w-24 sm:h-24 w-16 h-16 bg-blue-500  mb-10" style={{ aspectRatio: '1 / 1' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-arrow-up" style={{ transform: 'scale(0.5)' }}>
+              <line x1="12" y1="19" x2="12" y2="5"></line>
+              <polyline points="5 12 12 5 19 12"></polyline>
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
